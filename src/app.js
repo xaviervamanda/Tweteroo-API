b0574628a626aa65f3b8728a1d08edae397f3625
xaviervamanda <amandavxavier@gmail.com>
2023-04-08 18:08:14 -0300

    Requisito: GET /tweets

--- src/app.js
+++ src/app.js
@@ -9,20 +9,23 @@ app.use(express.json());
 const PORT = 5000;
 const users = [];
 const tweets = [];
-let userName = "";
-let userAvatar = "";
-let userTweet = "";
+// let userName = "";
+const userName = "amanda";
+
 
 app.post ("/sign-up", (req, res) => {
     const {username, avatar} = req.body;
-    if (!username || !avatar || typeof(username) !== "string" || typeof(avatar) !== "string"){
+    if (!username || !avatar){
         return res.status(400).send("Todos os campos são obrigatórios!");
     }
-    users.push(username);
-    userName = username;
-    userAvatar = avatar;
+    if (typeof(username) !== "string" || typeof(avatar) !== "string"){
+        res.sendStatus(400);
+    }
+    // userName = username;
+    users.push({username, avatar});
+    console.log(users);
     res.status(201).send("OK");
-})
+});
 
 app.post ("/tweets", (req, res) => {
     const {tweet} = req.body;
@@ -33,8 +36,26 @@ app.post ("/tweets", (req, res) => {
     if (!tweet){
         return res.status(400).send("Todos os campos são obrigatórios!");
     }
-    tweets.push(tweet);
+    if (typeof(tweet) !== "string"){
+        res.sendStatus(400);
+    }
+    tweets.push({username: user, tweet});
+    console.log(tweets)
     res.status(201).send("OK");
-})
+});
+
+app.get ("/tweets", (req, res) => {
+    const arrayTweets = tweets.slice(-10);
+    const updateTweets = [];
+    if (tweets.length === 0){
+        return res.send(tweets);
+    }
+    arrayTweets.map ((t) => {
+        const element = users.find ((u) => u.username === t.username)
+        t.avatar = element.avatar
+        updateTweets.push(t)
+    });
+    res.send (updateTweets);
+});
 
 app.listen (PORT, () => `Servidor rodando na porta ${PORT}`);
